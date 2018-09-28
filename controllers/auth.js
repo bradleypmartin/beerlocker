03e75d6fd1b3acc16b07ed68be5dbccaa57b4cf8
bradleypmartin <brad.martin@ibotta.com>
2018-09-28 15:25:35 -0600

    emptying basic strategy from auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,31 +1,31 @@
 // Load required packages
 var passport = require('passport');
-var BasicStrategy = require('passport-http').BasicStrategy;
+// var BasicStrategy = require('passport-http').BasicStrategy;
 var User = require('../models/user');
 var DigestStrategy = require('passport-http').DigestStrategy;
 
 
-passport.use(new BasicStrategy(
-  function(username, password, callback) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return callback(err); }
-
-      // No user found with that username
-      if (!user) { return callback(null, false); }
-
-      // Make sure the password is correct
-      user.verifyPassword(password, function(err, isMatch) {
-        if (err) { return callback(err); }
-
-        // Password did not match
-        if (!isMatch) { return callback(null, false); }
-
-        // Success
-        return callback(null, user);
-      });
-    });
-  }
-));
+// passport.use(new BasicStrategy(
+//   function(username, password, callback) {
+//     User.findOne({ username: username }, function (err, user) {
+//       if (err) { return callback(err); }
+//
+//       // No user found with that username
+//       if (!user) { return callback(null, false); }
+//
+//       // Make sure the password is correct
+//       user.verifyPassword(password, function(err, isMatch) {
+//         if (err) { return callback(err); }
+//
+//         // Password did not match
+//         if (!isMatch) { return callback(null, false); }
+//
+//         // Success
+//         return callback(null, user);
+//       });
+//     });
+//   }
+// ));
 
 passport.use(new DigestStrategy(
   { qop: 'auth' },
